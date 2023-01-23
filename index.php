d758d503ce64c32b05bf8401206774c78969d203
Nelutato <purepeper@gmail.com>
2023-01-23 17:24:04 +0100

    add

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,29 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
-    <link href="CSS/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/style.css">
     <!-- Scripts -->
     <script src="JS/script.js"></script>
+    <script src="JS/bootstrap.bundle.min.js"></script>
 
     <title>Portfolio</title>
 </head>
 <body >
-    <header class="bg-primary">
-        <img src="img/selfie.png" >
-        <nav>
-            <ol>
-                <ul class="menu_link" >
-                    Prace
-                </ul>
-                <ul class="menu_link" >
-                        O mnie
-                </ul>
-                <ul class="menu_link">
-                        Kontakt
-                </ul>
-            </ol>
-        </nav>
-    </header>
+    <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid">
+            <img src="img/selfie.png" class="fluid-image navbar-brand" >
+            
+            <div class="justify-content-center">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">O mnie</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Projekty</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Kontakt</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
 
     <div class="container">
 
